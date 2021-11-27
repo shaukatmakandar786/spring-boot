810e45c30758690ef1a75806bcbfba0056147e67
Shaukat Makandar <shaukatmakandar786@gmail.com>
2021-11-27 18:41:16 +0530

    added one to many relationship mapping using jpa example

---
+++ spring-jpa-one-to-many-relational-mapping/src/main/java/com/springjpaexample2/SpringJpaOneToManyRelationalMappingApplication.java
@@ -0,0 +1,13 @@
+package com.springjpaexample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaOneToManyRelationalMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaOneToManyRelationalMappingApplication.class, args);
+	}
+
+}
---
+++ spring-jpa-one-to-many-relational-mapping/src/test/java/com/springjpaexample2/SpringJpaOneToManyRelationalMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjpaexample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaOneToManyRelationalMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
