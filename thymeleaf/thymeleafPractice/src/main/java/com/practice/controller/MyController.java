14fd9429db79462790a910dd01d30876b611b894
shaukat makandar <shaukatmakandar786@gmail.com>
2021-12-22 22:12:13 +0530

    updated thymeleaf

--- thymeleaf/thymeleafPractice/src/main/java/com/practice/controller/MyController.java
+++ thymeleaf/thymeleafPractice/src/main/java/com/practice/controller/MyController.java
@@ -1,9 +1,12 @@
 package com.practice.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.expression.Lists;
 
 @Controller
 public class MyController {
@@ -18,6 +21,35 @@ public class MyController {
 		
 		return "about";
 	
+	}
+	
+	@GetMapping("/iterator")
+	public String iterator(Model model)
+	{
+		List<String> names = List.of("shaukat","wasim","asif","zahid");
+		model.addAttribute("names",names);
+		return "iterator";
+	}
+	
+	//Handler for conditional statement
+	@GetMapping("/condition")
+	public String conditionalHandler(Model model)
+	{
+		
+		System.out.println("conditional handler executed...");
+		model.addAttribute("isActive",false);
+		model.addAttribute("gender","M");
+		
+		List<Integer> list = List.of(6,8,9,6,7);
+		model.addAttribute("mylist",list);
+		return "condition";
+	}
+	
+	//handler for including fregments
+	@GetMapping("/service")
+	public String serviceHandler(Model m)
+	{
+		return "service";
 	}
 
 }
